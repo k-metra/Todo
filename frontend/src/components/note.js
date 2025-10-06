93196411798cba4445fac30bd87437c89e89c40a
k-metra <metrakurt@gmail.com>
2025-10-06 14:43:33 +0800

    feat: implement note deletion and fetching functionality with authentication checks

--- frontend/src/components/note.js
+++ frontend/src/components/note.js
@@ -1,9 +1,65 @@
-export default function Note({ title, content, date, onClick }) {
+import { useState } from "react";
+import Icon from "./icon";
+import getCsrf from "../utils/getCsrf";
+
+export default function Note({ title, content, date, onClick, note_id }) {
+    const [hovering, setHovering] = useState(false);
+
+    const handleDelete = async (e) => {
+        e.stopPropagation();
+
+        const sessionToken = sessionStorage.getItem("session_token");
+
+        if (!sessionToken) {
+            alert("You must be logged in to delete notes.");
+            window.location.reload();
+            return;
+        }
+
+        const csrfToken = getCsrf();
+        const endpoint = 
+            `http://localhost:8000/note/notes/edit/?session_token=${sessionToken}&note_id=${note_id}`;
+
+        await fetch(endpoint, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+        }).then(response => {
+            if (!response.ok) {
+                console.error("Something went wrong trying to delete the note.")
+                response.json().then(data => console.error(data));
+                return null;
+            } else {
+                console.log("Successfully deleted note.")
+                window.location.reload();
+            }
+        }).then(data => {
+            if (data) {
+                console.log(data);
+            }
+        })
+    }
+
     return (
-        <div onClick={onClick} className="note cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-out border border-black/10 rounded-md p-4 bg-white">
+        <div onClick={onClick} className="note cursor-pointer hover:shadow-lg transition-shadow duration-200 ease-out border border-black/10 rounded-md p-4 bg-white"
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+        >
             <h2 className="font-semibold text-lg mb-2 text-black/80">{title}</h2>
             <p className="text-black/70 mb-4 line-clamp-3">{content}</p>
             <p className="text-sm text-black/50">{new Date(date).toLocaleDateString()}</p>
+
+            {hovering &&
+                <div className="delete-btn-container w-full flex justify-end mt-2">
+                    <button className="bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600 transition-colors duration-200 ease-out hover:-translate-y-[1px]"
+                        onClick={handleDelete}
+                    >
+                        <Icon icon="fa fa-trash" className="w-4 h-4" />
+                    </button>
+                </div>
+            }
         </div>
     );
 }
--- frontend/src/pages/homepages/notes.js
+++ frontend/src/pages/homepages/notes.js
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Divider from "../../components/divider";
 import Note from "../../components/note";
 import Icon from "../../components/icon";
 
+import getCsrf from "../../utils/getCsrf";
+
 export default function Notes() {
+    const [fetchedNotes, setFetchedNotes] = useState([])
     const [focused, setFocused] = useState(false);
-    const [titleFocused, setTitleFocused] = useState(false);
 
     const handleUnfocus = (e) => {
         if (e.relatedTarget && e.relatedTarget.id === "noteTitle") {
@@ -18,6 +20,88 @@ export default function Notes() {
         setFocused(false);
     }
 
+    useEffect(() => {
+        const csrfToken = getCsrf();
+
+        const token = sessionStorage.getItem("session_token");
+        if (!token) {
+            alert("You must be logged in to view notes.");
+            window.location.reload();
+            return;
+        }
+
+        async function fetchNotes() {
+            await fetch('http://localhost:8000/note/notes/get?session_token=' + token, {
+                method: 'GET',
+            }).then(response => {
+                if (!response.ok) {
+                    console.error("Something went wrong trying to fetch the notes.")
+                    return null;
+                } else {
+                    console.log("Successfully fetched notes.")
+                    return response.json();
+                }
+            }).then(data => {
+                if (data) {
+                    console.log(data);
+                    setFetchedNotes(data);
+                }
+            })
+        }
+
+        fetchNotes();
+    }, []);
+    
+    async function createNote() {
+        const title = document.getElementById("noteTitle").value;
+        const content = document.getElementById("noteDesc").value;
+        const sessionToken = sessionStorage.getItem("session_token");
+
+        if (!sessionToken) {
+            alert("You must be logged in to create a note.");
+            window.location.reload();
+            return;
+        }
+
+        if (title === "" && content === "") {
+            return;
+        }
+
+        if (title.length > 100) {
+            alert("Title cannot be longer than 100 characters.");
+            return;
+        }
+
+        console.log(getCsrf());
+        await fetch('http://localhost:8000/note/notes/', {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCsrf(),
+                },
+            method: 'POST',
+            body: JSON.stringify({ title: title, content: content, session_token: sessionToken })
+        }).then(response => {
+            if (!response.ok) {
+                alert("Something went wrong trying to create the note.");
+                console.error("Something went wrong trying to create the note.");
+                response.json().then(data => console.error(data));
+                return null;
+            }
+            return response.json();
+        }).then(data => {
+            if (data) {
+                console.log("Successfully created new note.")
+                console.log("DATA AFTER RESPONSE: ")
+                console.log(data);
+                setFetchedNotes([...fetchedNotes, data]);
+
+                document.getElementById("noteTitle").value = "";
+                document.getElementById("noteDesc").value = "";
+                setFocused(false);
+            }
+        })
+    }
+
     return (
         <div id="createNotebar" className="flex flex-col w-full items-center">
             <div className="w-[90%] lg:w-[50%] md:w-[80%] sm:w-[20%] mt-10 p-3 rounded-md border border-black/20 shadow-md bg-white/50 transition-all flex flex-col duration-200 ease-out min-h-[10px]">
@@ -25,13 +109,15 @@ export default function Notes() {
                 <input id="noteDesc" onFocus={() => setFocused(true)} onBlur={handleUnfocus} className="w-full focus:outline-none" placeholder="Create a new note..."></input>
 
                 { focused && 
-                    <button className="self-end min-w-3 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all duration-200 ease-out hover:-translate-y-[2px]"><Icon className='' icon="fa fa-plus"/>&nbsp;Create Note</button>
+                    <button onClick={createNote} className="self-end min-w-3 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all duration-200 ease-out hover:-translate-y-[2px]"><Icon icon="fa fa-plus"/>&nbsp;Create Note</button>
                 }
             </div>
             <Divider />
 
             <div id="notes-grid" className="w-full grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4 p-4">
-                <Note title="Sample Note" content="This is a sample note." date={new Date()} onClick={() => alert("Note clicked!")} />
+                { fetchedNotes.map((note) => (
+                    <Note key={note.id} note_id={note.id} id={`${note.id}`} title={note.title} content={note.content} date={new Date(note.created_at)} onClick={() => alert("Note clicked!")} />
+                )) }
             </div>
         </div>
     );
