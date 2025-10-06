37ce2648265d2fdd6e7e5b5aef908ace618b0e54
k-metra <metrakurt@gmail.com>
2025-10-06 14:42:58 +0800

    add user field to note for one-to-all relationship foreignkey

--- backend/note/models.py
+++ backend/note/models.py
@@ -6,6 +6,7 @@ class Note(models.Model):
     content = models.TextField(max_length=2000)
     created_at = models.DateTimeField(auto_now_add=True)
     completed = models.BooleanField(default=False)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='notes')
 
     def __str__(self):
         return self.title
